226c3b19eaff6b18e56f4626a6a142d13a5cda44
michel <mihail.larionow2001@gmail.com>
2023-03-09 22:45:06 +0300

    Initial commit

---
+++ app/src/main/java/com/michel/friends_map/MainActivity.java
@@ -0,0 +1,14 @@
+package com.michel.friends_map;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

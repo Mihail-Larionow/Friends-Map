bfea5f232654a8733241bb5a9d71cd528fa98d16
michel <mihail.larionow2001@gmail.com>
2023-03-10 16:01:53 +0300

    Added a button that show current location

--- app/src/main/java/com/michel/friends_map/MainActivity.java
+++ app/src/main/java/com/michel/friends_map/MainActivity.java
@@ -7,12 +7,17 @@ import android.graphics.Color;
 import android.graphics.PointF;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.yandex.mapkit.Animation;
 import com.yandex.mapkit.MapKit;
 import com.yandex.mapkit.MapKitFactory;
 import com.yandex.mapkit.geometry.Point;
 import com.yandex.mapkit.layers.ObjectEvent;
+import com.yandex.mapkit.location.Location;
+import com.yandex.mapkit.location.LocationListener;
+import com.yandex.mapkit.location.LocationStatus;
 import com.yandex.mapkit.map.CameraPosition;
 import com.yandex.mapkit.map.CompositeIcon;
 import com.yandex.mapkit.map.IconStyle;
@@ -25,10 +30,12 @@ import com.yandex.runtime.image.ImageProvider;
 
 import java.util.Properties;
 
-public class MainActivity extends AppCompatActivity implements UserLocationObjectListener {
+public class MainActivity extends AppCompatActivity {
 
     private MapView mapView;
     private UserLocationLayer userLocationLayer;
+    private Point currentLocation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,14 +43,17 @@ public class MainActivity extends AppCompatActivity implements UserLocationObjec
         MapKitFactory.initialize(this);
         setContentView(R.layout.activity_main);
         mapView = (MapView)findViewById(R.id.mapview);
-        mapView.getMap().move(
-                new CameraPosition(new Point(0, 0), 11, 0, 0),
-                new Animation(Animation.Type.SMOOTH, 0), null);
+        ImageView locationButton = (ImageView)findViewById(R.id.locationButton);
+
+        locationButtonClick(locationButton);
+
         MapKit mapKit = MapKitFactory.getInstance();
+
+        updateLocation(mapKit);
+
         userLocationLayer = mapKit.createUserLocationLayer(mapView.getMapWindow());
         userLocationLayer.setVisible(true);
-        userLocationLayer.setHeadingEnabled(true);
-        userLocationLayer.setObjectListener(this);
+
     }
 
     @Override
@@ -60,24 +70,36 @@ public class MainActivity extends AppCompatActivity implements UserLocationObjec
         mapView.onStart();
     }
 
-    @Override
-    public void onObjectAdded(UserLocationView userLocationView) {
-        userLocationLayer.setAnchor(
-                new PointF((float)(mapView.getWidth() * 0.5), (float)(mapView.getHeight() * 0.5)),
-                new PointF((float)(mapView.getWidth() * 0.5), (float)(mapView.getHeight() * 0.83)));
+    private void updateLocation(MapKit mapKit){
+        mapKit.createLocationManager().requestSingleUpdate(new LocationListener() {
+            @Override
+            public void onLocationUpdated(@NonNull Location location) {
+                Log.d("TagCheck", "LocationUpdated " + location.getPosition().getLongitude());
+                Log.d("TagCheck", "LocationUpdated " + location.getPosition().getLatitude());
+                currentLocation = location.getPosition();
+                mapView.getMap().move(
+                        new CameraPosition(location.getPosition(), 14.0f, 0.0f, 0.0f),
+                        new Animation(Animation.Type.SMOOTH, 1),
+                        null);
+            }
 
-        userLocationView.getPin().setIcon(ImageProvider.fromResource(
-                this, R.drawable.happy));
-        userLocationView.getAccuracyCircle().setFillColor(Color.BLUE & 0x99ffffff);
+            @Override
+            public void onLocationStatusUpdated(@NonNull LocationStatus locationStatus) {
+
+            }
+        });
     }
 
-    @Override
-    public void onObjectRemoved(UserLocationView view) {
-    }
-
-    @Override
-    public void onObjectUpdated(@NonNull UserLocationView userLocationView, @NonNull ObjectEvent objectEvent) {
-
+    private void locationButtonClick(ImageView locationButton){
+        locationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mapView.getMap().move(
+                        new CameraPosition(currentLocation, 14.0f, 0.0f, 0.0f),
+                        new Animation(Animation.Type.SMOOTH, 1),
+                        null);
+            }
+        });
     }
 
 }

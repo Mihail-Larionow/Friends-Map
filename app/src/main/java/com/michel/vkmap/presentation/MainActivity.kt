2bdf19d4bb0d5253bca33b48df4b6c8e8479aac8
Mihail <mihail.larionow2001@gmail.com>
2023-12-20 21:23:01 +0300

    Add some logs

--- app/src/main/java/com/michel/vkmap/presentation/MainActivity.kt
+++ app/src/main/java/com/michel/vkmap/presentation/MainActivity.kt
@@ -1,43 +1,22 @@
 package com.michel.vkmap.presentation
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.michel.vkmap.ui.theme.VKMapTheme
+import android.util.Log
+import com.michel.vkmap.R
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 
-class MainActivity : ComponentActivity() {
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            VKMapTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
+
+        Log.d("MainActivity", "onCreate()")
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    VKMapTheme {
-        Greeting("Android")
-    }
-}
--- app/src/main/java/com/michel/vkmap/presentation/MainViewModel.kt
+++ app/src/main/java/com/michel/vkmap/presentation/MainViewModel.kt
@@ -1,7 +1,17 @@
 package com.michel.vkmap.presentation
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 
 class MainViewModel: ViewModel() {
-    
+
+    init{
+        Log.d("MainViewModel", "init()")
+    }
+
+    override fun onCleared() {
+        Log.d("MainViewModel", "onCleared()")
+        super.onCleared()
+    }
+
 }
